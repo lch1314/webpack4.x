65110a6feeb24e3d29397bf43a419d3cfc8bb3ea
liuchenghu <786973980@qq.com>
2019-04-12 14:25:58 +0800

    热模块更新

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log('哈哈哈aaaaaavvvvvv!!')
+// import './style.css';
+
+// var btn = document.createElement('button');
+// btn.innerHTML = '新增';
+// document.body.appendChild(btn);
+
+// btn.onclick = function() {
+//     var div = document.createElement('div');
+//     div.innerHTML = 'item';
+//     document.body.appendChild(div)
+// }
+
+import counter from './counter.js';
+import number from './number.js';
+counter();
+number();
+
+if(module.hot) {
+    // 如果number这个文件发生了变化，那么久会执行后面这个函数,让number()重新执行下
+    module.hot.accept('./number', () => {
+        let abc = document.getElementById('number');
+        document.body.removeChild(abc);
+        number();
+    })         
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
     mode: 'development', 
@@ -19,7 +20,9 @@ module.exports = {
         port: 8080,
         proxy: {
             '/api:': 'http://localhost:3000'
-        }
+        },
+        hot: true,               // 启用 webpack 的模块热替换特性
+        hotOnly: true            // 即使HMR功能不生效，也不让浏览器自动刷新
     },
     module: {
         rules: [
@@ -51,19 +54,31 @@ module.exports = {
                 ]
             },
             {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader', 
+                    'postcss-loader'
+                ]
+            },
+            {
                 test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
                 use: ['file-loader']
             }
         ]
     },
-    plugins: [new HtmlWebpackPlugin(
-        {
-            template: 'src/index.html',
-            title: 'test App',
-            filename: 'index.html',
-            minify: {
-                collapseWhitespace: true
+    plugins: [
+        new HtmlWebpackPlugin(
+            {
+                template: 'src/index.html',
+                title: 'test App',
+                filename: 'index.html',
+                minify: {
+                    collapseWhitespace: true
+                }
             }
-        }
-    ), new CleanWebpackPlugin()]
+        ), 
+        new CleanWebpackPlugin(), 
+        new webpack.HotModuleReplacementPlugin()        // webapck内置插件
+    ]
 }
